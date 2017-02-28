34a9bbe9a41b8b75c037428ceed043fbd54066b7
Celena Williams <celena.c.williams@gmail.com>
2017-02-28 12:23:25 -0500

    Add files via upload

--- BookPractice/Biblio.java
+++ BookPractice/Biblio.java
@@ -1,1 +1,21 @@
+/*
+	Chapter 2:	Programming Assignment 1
+	Programmer:	C. Williams
+	Date:		September 4, 2007
+	Filename:	Biblio.java
+	Purpose:	This project displays a wthe name, address and
+				web page address as a console application.
+*/
 
+public class Biblio {
+   public static void main (String[] args) {
+        System.out.println ();
+        System.out.println ("Java Programming");
+        System.out.println ();
+        System.out.println ("\t Shelly, Gary B. \n\t Cashman, Thomas J. \n\t " +
+                            "Starks, Joy L. \n\t Mick, Micheal L.");
+        System.out.println ("\t COPYRIGHT 2006 \n\t Thomson Course " +
+                            "Technology, a division of Thomson Learning, Inc.");
+        System.out.println ();
+  }//END public static void main (String[] args)
+}//END public class Biblio
