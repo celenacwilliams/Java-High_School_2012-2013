a48c18797cde56cd5fbe457c58afc5b1eca2a659
Celena Williams <celena.c.williams@gmail.com>
2017-02-28 18:26:54 -0500

    Add files via upload

--- EscapeCharacters/MIC.java
+++ EscapeCharacters/MIC.java
@@ -1,1 +1,20 @@
+/*
+	Chapter 2:	Escape Characters
+	Programmer:	C.Williams
+	Date:		February 20, 2013
+	Filename: 	MIC.java
+	Purpose:	This project displays the Middle Illinois College's
+			letters on a splash screen.
+*/
 
+public class MIC {
+    public static void main (String [] args) {
+        System.out.println ("\tMM            MM \tIIIIIIIIIIIII \t         CCCCCCCC");
+        System.out.println ("\tM M          M M \t      I            \tCC");
+        System.out.println ("\tM   M       M  M	      I	          \tCC");
+        System.out.println ("\tM    M     M   M\t      I           \tCC");
+        System.out.println ("\tM     M   M    M\t      I           \tCC");
+        System.out.println ("\tM      M M     M\t      I           \tCC");
+        System.out.println ("\tM       M      M\tIIIIIIIIIIIII\t         CCCCCCCC");
+    }//END public static void main (String [] args)
+}//END public class MIC
