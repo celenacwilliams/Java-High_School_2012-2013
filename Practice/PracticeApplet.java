0c5fb89f30005f92a334b464c078fda40fe1f5f4
Celena Williams <celena.c.williams@gmail.com>
2017-02-28 18:30:17 -0500

    Add files via upload

--- Practice/PracticeApplet.java
+++ Practice/PracticeApplet.java
@@ -1,1 +1,33 @@
+//Celena
 
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class PracticeApplet extends Applet implements ActionListener {
+    Label gradeLabel = new Label ("Grades");
+    Label gradePromptLabel = new Label ("Enter Grade:");
+            TextField gradePromptField = new TextField (5);
+    Button enterButton = new Button ("Enter");
+    Label outputLabel = new Label ("                              ");
+
+    public void init () {
+        add (gradeLabel);
+        add (gradePromptLabel);
+        add (gradePromptField);
+        gradePromptField.requestFocus ();
+        add (enterButton);
+        enterButton.addActionListener (this);
+        add (outputLabel);
+    }//END public void init ()
+
+    public void actionPerformed (ActionEvent e) {
+        int grade = Integer.parseInt (gradePromptField.getText ());
+
+        if (grade >= 70)
+            outputLabel.setText ("You passed");
+        else
+            if (grade <= 69)
+                outputLabel.setText ("You failed");
+    }//END public void actionPerformed (ActionEvent e)
+}//END public class PracticeApplet
