7f55a12809a997d6d93146288bc4a9fb2a321ecc
Celena Williams <celena.c.williams@gmail.com>
2017-02-28 18:29:56 -0500

    Add files via upload

--- MyColorApplet/MyColorApplet.java
+++ MyColorApplet/MyColorApplet.java
@@ -1,1 +1,18 @@
+/*
 
+    Chapter 2:	My Color Applet
+    Programmer:	C.Williams
+    Date:		February 20, 2013
+    Filename: 	MyColorApplet.java
+    Purpose:	This project displays a colored background and black text.
+*/
+
+import java.awt.*;
+import java.applet.*;
+
+public class MyColorApplet extends Applet {
+    public void paint (Graphics g) {
+        g.drawString ("This is a color test.", 25, 30);
+        setBackground (Color.orange);
+    }//END public void paint (Graphics g)
+}//END public class MyColorApplet
